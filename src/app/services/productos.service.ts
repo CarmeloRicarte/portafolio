a45f6f75d5d7f70dd7a45d07fdc53652d273d049
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2018-08-10 17:37:38 +0200

    Se crea servicio ProductosService para cargar los datos de cada producto y el id de cada uno conectando con Firebase.

---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+  cargando = true;
+  productos: Producto [] = [];
+  constructor( private http: HttpClient ) {
+    this.cargarProductos();
+  }
+
+  private cargarProductos() {
+    this.http.get('https://carmelo-s-portafolio.firebaseio.com/productos_idx.json')
+      .subscribe( (resp: Producto[]) => {
+          this.productos = resp;
+          setTimeout(() => {
+          this.cargando = false;
+          }, 600);
+      });
+  }
+
+  getProducto( id: string ) {
+    return this.http.get(`https://carmelo-s-portafolio.firebaseio.com/productos/${ id }.json`);
+  }
+}
