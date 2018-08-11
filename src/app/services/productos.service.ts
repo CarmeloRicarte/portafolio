d95c626cd7f59e7ad8c8851503ef701de2f134f2
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2018-08-11 19:12:54 +0200

    Fin Sección 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -2,27 +2,58 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
 
+
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
   cargando = true;
   productos: Producto [] = [];
+  productosFiltrado: Producto [] = [];
   constructor( private http: HttpClient ) {
     this.cargarProductos();
   }
 
   private cargarProductos() {
-    this.http.get('https://carmelo-s-portafolio.firebaseio.com/productos_idx.json')
-      .subscribe( (resp: Producto[]) => {
+
+    return new Promise( (resolve, reject) => {
+      this.http.get('https://carmelo-s-portafolio.firebaseio.com/productos_idx.json')
+        .subscribe( (resp: Producto[]) => {
           this.productos = resp;
-          setTimeout(() => {
           this.cargando = false;
-          }, 600);
-      });
+          resolve();
+        });
+    });
   }
 
   getProducto( id: string ) {
     return this.http.get(`https://carmelo-s-portafolio.firebaseio.com/productos/${ id }.json`);
+  }
+
+  buscarProducto ( termino: string ) {
+    if (this.productos.length === 0 ) {
+      // cargar productos
+      this.cargarProductos().then( () => {
+        // ejecutar después de tener los productos
+        // aplicar filtro
+        this.filtrarProductos( termino );
+      });
+    } else {
+      // aplicar el filtro
+      this.filtrarProductos( termino );
+    }
+  }
+  private filtrarProductos( termino: string ) {
+    this.productosFiltrado = [];
+    termino = termino.toLocaleLowerCase();
+    this.productos.forEach( prod => {
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+      if (prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0) {
+        this.productosFiltrado.push( prod );
+      }
+    });
+
   }
 }
