955a6c3856c904d6186cc2e62d6a2dd4e9036e68
CarmeloRicarte <carmeloricarterocamora@gmail.com>
2019-04-06 11:44:47 +0200

    Versión 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// Componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
